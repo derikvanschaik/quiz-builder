907d8a58ccce267e8b76a1dfebd4dc235ed05ccb
Derik van Schaik <derikvanschaik@gmail.com>
2023-11-18 23:42:37 -0800

    user can create multiple select quiz card

--- src/components/QuizBuilder.vue
+++ src/components/QuizBuilder.vue
@@ -5,9 +5,14 @@ import { ref, reactive } from 'vue';
 const state = reactive({
   newQuestion: '', 
   newAnswer: '',
+  // used in multiple select and multiple choice
+  additional: [],
+  isAnswer: false, 
+
   title: '',
   step: 1,
   cardIndex: 0,
+  answerType: 'basic', // basic, multiple select, multiple choice
   quizCards: [],
 });
 
@@ -21,10 +26,17 @@ const prevStep = () => {
 };
 
 const addCard = () => {
-  state.quizCards.push({ question: state.newQuestion, answer: state.newAnswer});
+  if(state.answerType === 'basic'){
+    state.quizCards.push({ question: state.newQuestion, answer: state.newAnswer, answerType: 'basic'});
+  }else{
+      state.quizCards.push({ question: state.newQuestion, answers: state.additional, answerType: 'multiple'});
+  }
   state.cardIndex = state.quizCards.length;
   state.newAnswer = '';
   state.newQuestion = '';
+  state.answerType = 'basic';
+  state.additional = [];
+
 };
 
 const prevCard = () => {
@@ -32,6 +44,27 @@ const prevCard = () => {
 }
 const nextCard = () =>{
   state.cardIndex++;
+}
+
+const addMultipleAnswer = () =>{
+  state.additional.push({ answer: state.newAnswer, isAnswer: state.isAnswer })
+  state.newAnswer = '';
+  state.isAnswer = false;
+}
+
+const disableCreateButton =() =>{
+  // don't allow to create when browsing cards
+  if(state.cardIndex < state.quizCards.length ){
+    return false;
+  }
+
+  if(state.answerType === 'basic'){
+    return state.newAnswer === '' || state.newQuestion === '';
+  }
+  // must have more than one answer and must have at least one correct answer
+  const hasAtLeastOneAnswer = state.additional.length > 0;
+  const hasAtLeastOneAnswerMarkedAsCorrect = state.additional.filter(a => a.isAnswer === true).length > 0;
+  return state.newQuestion === '' || !(hasAtLeastOneAnswer && hasAtLeastOneAnswerMarkedAsCorrect);
 }
 
 // const removeCard = (index: number) => {
@@ -60,36 +93,73 @@ const submitQuiz = () => {
       <!-- Step 2: Create Quiz Card -->
       <div class="quiz-header">
         <h3 class="quiz-header-title">Step 2: Create Quiz Cards | <span class="progress-chip">Question [{{state.cardIndex + 1}}]/{{state.quizCards.length}}</span> </h3>
-        <button @click="addCard" class="create-card contrast outline" :disabled="state.newAnswer === '' || state.newQuestion === ''" >Create New Card</button>
+        <button @click="addCard" class="create-card contrast outline" :disabled="disableCreateButton()" >Create New Card</button>
       </div>
-      <div class="quiz-card">
+
+      <!-- CARD THAT ALREADY EXISTS IN DECK -->
+      <div class="quiz-card" v-if="state.cardIndex < state.quizCards.length">
         <label>Question:</label>
         <input 
-          v-if="state.cardIndex < state.quizCards.length" 
           v-model="state.quizCards[state.cardIndex].question" 
-          placeholder="Enter question" />
-        <input 
-          v-else 
-          v-model="state.newQuestion" 
           placeholder="Enter question" />
         
         <label>Answer:</label>
         <input 
-          v-if="state.cardIndex < state.quizCards.length" 
           v-model="state.quizCards[state.cardIndex].answer" 
           placeholder="Enter answer" />
-        <input 
-          v-else
-          v-model="state.newAnswer" 
-          placeholder="Enter answer" />
       </div>
+
+      <!-- NEW CARD -->
+      <div v-else>
+        <label>Question:</label>
+          <input 
+            v-model="state.newQuestion" 
+            placeholder="Enter question" />
+        <!-- Choose Answer type -->
+        <label>Answer Type:</label>
+        <select v-model="state.answerType">
+          <option value="basic">Basic</option>
+          <option value="multiple">Multiple</option>
+        </select>
+
+        <!-- basic question : one question and answer -->
+        <div v-if="state.answerType === 'basic'">
+          <label>Answer</label>
+          <input 
+            v-model="state.newAnswer" 
+            placeholder="Enter answer" />
+        </div>
+        <!-- multiple choice -->
+        <div v-else-if="state.answerType === 'multiple'">
+          <label>Answers:</label>
+          <div v-for="(add, index) in state.additional" :key="add" class="multiple-select-answer">
+            <input type="checkbox" v-model="state.additional[index].isAnswer" />
+            <input v-model="state.additional[index].answer" />
+          </div>
+
+          <fieldset>
+            <label for="is-answer">
+              <input type="checkbox" id="is-answer" name="is-answer" v-model="state.isAnswer">
+              Select If Correct
+            </label>
+            <input 
+                v-model="state.newAnswer" 
+                placeholder="Enter new answer" />
+          </fieldset>
+
+
+          <button @click="addMultipleAnswer">Add Answer</button>
+        </div>
+      </div>
+
       <div class="actions">
         <button :disabled="state.cardIndex <= 0" @click="prevCard" class="action-button contrast outline">Previous Question</button>
         <button :disabled="state.cardIndex >= state.quizCards.length" @click="nextCard" class="action-button contrast outline">Next Question</button>
         <button @click="submitQuiz" class="action-button contrast outline" :disabled="state.quizCards.length === 0">Submit Quiz</button>
       </div>
     </div>
-    <!-- Created card! -->
+
+    <!-- Created cards! -->
     <div v-else>
       <h4>Congrats on building your quiz!</h4>
       <div v-for="quiz in state.quizCards" :key="quiz.question">
@@ -138,4 +208,18 @@ const submitQuiz = () => {
   font-weight: normal;
   font-size: 18px;
 }
+.multiple-select-answer{
+  display: flex;
+  align-items: start;
+}
+/* .checkbox-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+}
+.select-label{
+  font-size: large;
+  margin-right: 10px;
+} */
 </style>
