8940f37918d74512f7966a07ef2acdede938bcdf
Derik van Schaik <derikvanschaik@gmail.com>
2023-11-19 12:02:24 -0800

    add types to quiz

--- src/components/QuizBuilder.vue
+++ src/components/QuizBuilder.vue
@@ -1,85 +1,96 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
+import { ref, reactive } from 'vue'
+import { type AnswerType, type MultipleAnswerType, type Quiz } from './QuizTypes'
 
 // State
 const state = reactive({
-  newQuestion: '', 
+  newQuestion: '',
   newAnswer: '',
   // used in multiple select and multiple choice
-  additional: [],
-  isAnswer: false, 
+  additional: ref<MultipleAnswerType[]>([]),
+  isAnswer: false,
 
   title: '',
   step: 1,
   cardIndex: 0,
-  answerType: 'basic', // basic, multiple select, multiple choice
-  quizCards: [],
-});
+  answerType: ref<AnswerType>('basic'),
+  quizCards: ref<Quiz[]>([])
+})
 
-const initState = () =>{
-  state.newQuestion = '';
-  state.newAnswer = '';
-  state.additional = [];
-  state.isAnswer = false;
-  state.title = '';
-  state.step = 1;
-  state.cardIndex = 0;
-  state.answerType = 'basic';
-  state.quizCards = [];
+const initState = () => {
+  state.newQuestion = ''
+  state.newAnswer = ''
+  state.additional = []
+  state.isAnswer = false
+  state.title = ''
+  state.step = 1
+  state.cardIndex = 0
+  state.answerType = 'basic'
+  state.quizCards = []
 }
 
 // Methods
 const nextStep = () => {
-  state.step = 2;
-};
+  state.step = 2
+}
 
 const prevStep = () => {
-  state.step = 1;
-};
+  state.step = 1
+}
 
 const addCard = () => {
-  if(state.answerType === 'basic'){
-    state.quizCards.push({ question: state.newQuestion, answer: state.newAnswer, answerType: 'basic'});
-  }else{
-    state.quizCards.push({ question: state.newQuestion, answers: state.additional, answerType: 'multiple'});
+  if (state.answerType === 'basic') {
+    state.quizCards.push({
+      question: state.newQuestion,
+      answer: state.newAnswer,
+      answers: [],
+      answerType: 'basic'
+    })
+  } else {
+    state.quizCards.push({
+      question: state.newQuestion,
+      answer: '',
+      answers: state.additional,
+      answerType: 'multiple'
+    })
   }
 
   console.log(state.quizCards)
 
-  state.cardIndex = state.quizCards.length;
-  state.newAnswer = '';
-  state.newQuestion = '';
-  state.answerType = 'basic';
-  state.additional = [];
-
-};
+  state.cardIndex = state.quizCards.length
+  state.newAnswer = ''
+  state.newQuestion = ''
+  state.answerType = 'basic'
+  state.additional = []
+}
 
 const prevCard = () => {
-  state.cardIndex--;
+  state.cardIndex--
 }
-const nextCard = () =>{
-  state.cardIndex++;
+const nextCard = () => {
+  state.cardIndex++
 }
 
-const addMultipleAnswer = () =>{
+const addMultipleAnswer = () => {
   state.additional.push({ answer: state.newAnswer, isAnswer: state.isAnswer })
-  state.newAnswer = '';
-  state.isAnswer = false;
+  state.newAnswer = ''
+  state.isAnswer = false
 }
 
-const disableCreateButton =() =>{
+const disableCreateButton = () => {
   // don't allow to create when browsing cards
-  if(state.cardIndex < state.quizCards.length ){
-    return false;
+  if (state.cardIndex < state.quizCards.length) {
+    return false
   }
 
-  if(state.answerType === 'basic'){
-    return state.newAnswer === '' || state.newQuestion === '';
+  if (state.answerType === 'basic') {
+    return state.newAnswer === '' || state.newQuestion === ''
   }
   // must have more than one answer and must have at least one correct answer
-  const hasAtLeastOneAnswer = state.additional.length > 0;
-  const hasAtLeastOneAnswerMarkedAsCorrect = state.additional.filter(a => a.isAnswer === true).length > 0;
-  return state.newQuestion === '' || !(hasAtLeastOneAnswer && hasAtLeastOneAnswerMarkedAsCorrect);
+  const hasAtLeastOneAnswer = state.additional.length > 0
+  const hasAtLeastOneAnswerMarkedAsCorrect =
+    state.additional.filter((a) => a.isAnswer === true).length > 0
+  return state.newQuestion === '' || !(hasAtLeastOneAnswer && hasAtLeastOneAnswerMarkedAsCorrect)
 }
 
 // const removeCard = (index: number) => {
@@ -88,18 +99,18 @@ const disableCreateButton =() =>{
 
 const submitQuiz = () => {
   // Implement logic to submit the quiz data
-  state.step = 3;
+  state.step = 3
 
-  // TODO: CREATE BACKEND 
-  localStorage.setItem("quiz", JSON.stringify(state.quizCards) );
-
-};
+  // TODO: CREATE BACKEND
+  localStorage.setItem('quiz', JSON.stringify(state.quizCards))
+}
 </script>
-
 
 <template>
   <div>
-    <button v-if="state.step == 2" @click="prevStep" class="back-button contrast outline">Back</button>
+    <button v-if="state.step == 2" @click="prevStep" class="back-button contrast outline">
+      Back
+    </button>
 
     <div v-if="state.step === 1">
       <!-- Step 1: Create Title of Quiz -->
@@ -110,52 +121,57 @@ const submitQuiz = () => {
     <div v-else-if="state.step === 2">
       <!-- Step 2: Create Quiz Card -->
       <div class="quiz-header">
-        <h3 class="quiz-header-title">Step 2: Create Quiz Cards
-          <span class="progress-chip" v-if="state.cardIndex < state.quizCards.length"> Question [{{state.cardIndex + 1}}]/{{state.quizCards.length}}</span> 
-          <span class="progress-chip" v-else>New Question</span> 
+        <h3 class="quiz-header-title">
+          Step 2: Create Quiz Cards
+          <span class="progress-chip" v-if="state.cardIndex < state.quizCards.length">
+            Question [{{ state.cardIndex + 1 }}]/{{ state.quizCards.length }}</span
+          >
+          <span class="progress-chip" v-else>New Question</span>
         </h3>
-        <button @click="addCard" class="create-card contrast outline" :disabled="disableCreateButton()" >Create New Card</button>
+        <button
+          @click="addCard"
+          class="create-card contrast outline"
+          :disabled="disableCreateButton()"
+        >
+          Create New Card
+        </button>
       </div>
 
       <!-- CARD THAT ALREADY EXISTS IN DECK -->
       <div class="quiz-card" v-if="state.cardIndex < state.quizCards.length">
         <!-- basic answer -->
-        <div v-if="state.quizCards[state.cardIndex].answerType === 'basic' ">
+        <div v-if="state.quizCards[state.cardIndex].answerType === 'basic'">
           <label>Question:</label>
-          <input 
-            v-model="state.quizCards[state.cardIndex].question"
-            placeholder="Enter question" />
-          
+          <input v-model="state.quizCards[state.cardIndex].question" placeholder="Enter question" />
+
           <label>Answer:</label>
-          <input 
-            v-model="state.quizCards[state.cardIndex].answer" 
-            placeholder="Enter answer" />
+          <input v-model="state.quizCards[state.cardIndex].answer" placeholder="Enter answer" />
         </div>
 
         <!-- multiple answer (multiple select or choice) -->
         <div v-else>
           <label>Question:</label>
-          <input 
-            v-model="state.quizCards[state.cardIndex].question"
-            placeholder="Enter question" />
-          
+          <input v-model="state.quizCards[state.cardIndex].question" placeholder="Enter question" />
+
           <label>Answers:</label>
-          <div v-for="(answer, index) in state.quizCards[state.cardIndex].answers" :key="answer" class="multiple-select-answer">
-            <input type="checkbox" v-model="state.quizCards[state.cardIndex].answers[index].isAnswer" />
+          <div
+            v-for="(answer, index) in state.quizCards[state.cardIndex].answers"
+            :key="answer"
+            class="multiple-select-answer"
+          >
+            <input
+              type="checkbox"
+              v-model="state.quizCards[state.cardIndex].answers[index].isAnswer"
+            />
             <input v-model="state.quizCards[state.cardIndex].answers[index].answer" />
           </div>
-          
-
-
         </div>
       </div>
 
       <!-- NEW CARD -->
       <div v-else>
         <label>Question:</label>
-          <input 
-            v-model="state.newQuestion" 
-            placeholder="Enter question" />
+        <input v-model="state.newQuestion" placeholder="Enter question" />
         <!-- Choose Answer type -->
         <label>Answer Type:</label>
         <select v-model="state.answerType">
@@ -166,9 +182,7 @@ const submitQuiz = () => {
         <!-- basic question : one question and answer -->
         <div v-if="state.answerType === 'basic'">
           <label>Answer</label>
-          <input 
-            v-model="state.newAnswer" 
-            placeholder="Enter answer" />
+          <input v-model="state.newAnswer" placeholder="Enter answer" />
         </div>
         <!-- multiple choice -->
         <div v-else-if="state.answerType === 'multiple'">
@@ -180,23 +194,38 @@ const submitQuiz = () => {
 
           <fieldset>
             <label for="is-answer">
-              <input type="checkbox" id="is-answer" name="is-answer" v-model="state.isAnswer">
+              <input type="checkbox" id="is-answer" name="is-answer" v-model="state.isAnswer" />
               Select If Correct
             </label>
-            <input 
-                v-model="state.newAnswer" 
-                placeholder="Enter new answer" />
+            <input v-model="state.newAnswer" placeholder="Enter new answer" />
           </fieldset>
-
 
           <button @click="addMultipleAnswer">Add Answer</button>
         </div>
       </div>
 
       <div class="actions">
-        <button :disabled="state.cardIndex <= 0" @click="prevCard" class="action-button contrast outline">Previous Question</button>
-        <button :disabled="state.cardIndex >= state.quizCards.length" @click="nextCard" class="action-button contrast outline">Next Question</button>
-        <button @click="submitQuiz()" class="action-button contrast outline" :disabled="state.quizCards.length === 0">Submit Quiz</button>
+        <button
+          :disabled="state.cardIndex <= 0"
+          @click="prevCard"
+          class="action-button contrast outline"
+        >
+          Previous Question
+        </button>
+        <button
+          :disabled="state.cardIndex >= state.quizCards.length"
+          @click="nextCard"
+          class="action-button contrast outline"
+        >
+          Next Question
+        </button>
+        <button
+          @click="submitQuiz()"
+          class="action-button contrast outline"
+          :disabled="state.quizCards.length === 0"
+        >
+          Submit Quiz
+        </button>
       </div>
     </div>
 
@@ -209,42 +238,42 @@ const submitQuiz = () => {
 </template>
 
 <style scoped>
-.quiz-header{
+.quiz-header {
   display: flex;
   align-items: start;
 }
-.quiz-header-title{
+.quiz-header-title {
   width: 80%;
   margin-right: 10px;
 }
-.create-card{
+.create-card {
   width: fit-content;
 }
 .quiz-card {
   margin-bottom: 10px;
 }
-.actions{
+.actions {
   display: flex;
 }
-.action-button{
+.action-button {
   width: 30%;
   margin-right: 10px;
 }
-.remove-card{
+.remove-card {
   background: red;
   width: fit-content;
 }
-.back-button{
+.back-button {
   width: fit-content;
   padding-left: 100px;
   padding-right: 100px;
 }
-.progress-chip{
+.progress-chip {
   font-style: italic;
   font-weight: normal;
   font-size: 18px;
 }
-.multiple-select-answer{
+.multiple-select-answer {
   display: flex;
   align-items: start;
 }
--- src/components/QuizTaker.vue
+++ src/components/QuizTaker.vue
@@ -1,82 +1,95 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue'
+import { type AnswerType, type MultipleAnswerType, type Quiz } from './QuizTypes'
 
-const state = reactive({ quiz: null, curCardIndex: 0 });
-const show = ref(false);
+const state = reactive({ quiz: ref<Quiz[]>([]), curCardIndex: 0 })
+const show = ref(false)
 
-const currentGuess = reactive({ selected: [] });
+const currentGuess = reactive({ selected: ref<boolean[]>([]) })
 
 // toggles the selected answer value
 const logMultipleAnswer = (answerIndex: number) => {
-  currentGuess.selected[answerIndex] = !currentGuess.selected[answerIndex];
-  
-};
+  currentGuess.selected[answerIndex] = !currentGuess.selected[answerIndex]
+}
 
 const checkMultipleAnswer = () => {
-  show.value = true;
-};
+  show.value = true
+}
 
 const nextCard = () => {
   // Increment the current card index
-  state.curCardIndex++;
+  state.curCardIndex++
 
   // Reset the show flag
-  show.value = false;
+  show.value = false
 
   // Reset the currentGuess for the next card
-  if(state.curCardIndex < state.quiz.length && state.quiz[state.curCardIndex].answerType === 'multiple'){
+  if (
+    state.curCardIndex < state.quiz.length &&
+    state.quiz[state.curCardIndex].answerType === 'multiple'
+  ) {
     // map state all to false
-    currentGuess.selected = state.quiz[state.curCardIndex].answers.map( a => false);
+    currentGuess.selected = state.quiz[state.curCardIndex].answers.map((a) => false)
     console.log(currentGuess.selected)
-  }else{
-    currentGuess.selected = [];
+  } else {
+    currentGuess.selected = []
   }
-  
-};
+}
 
 // TODO: remove this when backend is implemented
 onMounted(() => {
-  const quizJson = localStorage.getItem('quiz');
-  state.quiz = JSON.parse(quizJson);
+  const quizJson = localStorage.getItem('quiz')
+  if (quizJson) {
+    state.quiz = JSON.parse(quizJson)
+  }
 
   // map state all to false
- currentGuess.selected = state.quiz[state.curCardIndex].answers.map( a => false);
-});
+  currentGuess.selected = state.quiz[state.curCardIndex].answers.map((a) => false)
+})
 </script>
 
 <template>
-  <div v-if="state.quiz">
+  <div v-if="state.quiz.length > 0">
     <article :key="state.curCardIndex">
       <h4>{{ state.quiz[state.curCardIndex].question }}</h4>
       <!-- BASIC SHOW ANSWER -->
-      <p v-if="state.quiz[state.curCardIndex].answerType === 'basic'">
-        {{ state.quiz[state.curCardIndex].answer }}
-      </p>
+      <div v-if="state.quiz[state.curCardIndex].answerType === 'basic'">
+        <button v-if="!show" @click="show = true">See Answer</button>
+        <p v-if="show">{{ state.quiz[state.curCardIndex].answer }}</p>
+      </div>
       <!-- MULTIPLE SELECT -->
       <div v-else>
         <div v-for="(answer, index) in state.quiz[state.curCardIndex].answers" :key="answer.answer">
-            <div class="guess-row">
-                <input type="checkbox" @click="logMultipleAnswer(index)" />
-                <p 
-                        :class="{ 
-                        correct: show && (state.quiz[state.curCardIndex].answers[index].isAnswer),
-                        incorrect: show && !(state.quiz[state.curCardIndex].answers[index].isAnswer)
-                        }"
-                    >{{ answer.answer }}</p>
-                <p v-if="show">
-                    {{ currentGuess.selected[index] === state.quiz[state.curCardIndex].answers[index].isAnswer ? '1/1' : '0/1'}}
-                </p>
-            </div>
+          <div class="guess-row">
+            <input type="checkbox" @click="logMultipleAnswer(index)" />
+            <p
+              :class="{
+                correct: show && state.quiz[state.curCardIndex].answers[index].isAnswer,
+                incorrect: show && !state.quiz[state.curCardIndex].answers[index].isAnswer
+              }"
+            >
+              {{ answer.answer }}
+            </p>
+            <p v-if="show">
+              {{
+                currentGuess.selected[index] ===
+                state.quiz[state.curCardIndex].answers[index].isAnswer
+                  ? '1/1'
+                  : '0/1'
+              }}
+            </p>
+          </div>
         </div>
         <button @click="checkMultipleAnswer" v-if="!show">Check Answer</button>
       </div>
     </article>
-    <button @click="nextCard" v-if="state.curCardIndex < state.quiz.length -1 && (show || state.quiz[state.curCardIndex].answerType === 'basic')">Next</button>
+    <button @click="nextCard" v-if="state.curCardIndex < state.quiz.length - 1 && show">
+      Next
+    </button>
   </div>
 </template>
 
 <style scoped>
-
 .correct {
   color: green;
   font-weight: bold;
@@ -87,4 +100,3 @@ onMounted(() => {
   text-decoration: line-through;
 }
 </style>
-
